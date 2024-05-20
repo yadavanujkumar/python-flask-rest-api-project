c938062086c5c4ffe0c5f2ff3bcf2f569221045c
yadavanujkumar <thunderstruck9999@gmail.com>
2024-05-20 19:37:41 +0530

    first commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def helloworld():
+    print('Hello User')
 	if(request.method == 'GET'):
 		data = {"data": "Hello World"}
 		return jsonify(data)
